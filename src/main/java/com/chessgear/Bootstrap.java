06146ff8bdab60f455190207649e249393780882
Abso <rliu14@jhu.edu>
2015-10-08 14:31:19 -0400

    Periodic commit.

--- src/main/java/com/chessgear/Bootstrap.java
+++ src/main/java/com/chessgear/Bootstrap.java
@@ -1,12 +1,20 @@
 package com.chessgear;
 
+import static spark.Spark.*;
+
 /**
  * ChessGear main class.
  * Created by Ran on 10/8/2015.
  */
 public class Bootstrap {
 
+    /**
+     * Port for server to listen on.
+     */
+    private static final int PORT = 80;
+
     public static void main (String[] args) {
 
+        port(PORT);
     }
 }
