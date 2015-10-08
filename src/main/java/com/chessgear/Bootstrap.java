967c37ee34ef29370badef980012bc3df6d190ff
Abso <rliu14@jhu.edu>
2015-10-08 13:54:46 -0400

    Adding Source

---
+++ src/main/java/com/chessgear/Bootstrap.java
@@ -0,0 +1,12 @@
+package com.chessgear;
+
+/**
+ * ChessGear main class.
+ * Created by Ran on 10/8/2015.
+ */
+public class Bootstrap {
+
+    public static void main (String[] args) {
+
+    }
+}
---
+++ src/main/java/com/chessgear/data/GameTreeNode.java
@@ -0,0 +1,7 @@
+package com.chessgear.data;
+
+/**
+ * Created by Ran on 10/8/2015.
+ */
+public class GameTreeNode {
+}
