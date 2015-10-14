1e3f30ce358e341f4e5fdd3397c2b0100d4299eb
Abso <rliu14@jhu.edu>
2015-10-14 13:29:22 -0400

    Periodic commit.

---
+++ src/main/java/com/chessgear/data/PGNParser.java
@@ -0,0 +1,7 @@
+package com.chessgear.data;
+
+/**
+ * Created by Ran on 10/14/2015.
+ */
+public class PGNParser {
+}
--- src/main/java/com/chessgear/game/Move.java
+++ src/main/java/com/chessgear/game/Move.java
@@ -1,7 +1,11 @@
 package com.chessgear.game;
 
 /**
+ * Move class.
  * Created by Ran on 10/8/2015.
  */
 public class Move {
+
+    private Player whoMoved;
+
 }
