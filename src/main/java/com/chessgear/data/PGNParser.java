cab9b3da144438854d6cdd9487acdfe795a9a7b3
Abso <rliu14@jhu.edu>
2015-10-14 13:57:25 -0400

    Merge branch 'ran-boardstate'
    Conflicts:
    	.idea/workspace.xml

--- src/main/java/com/chessgear/data/PGNParser.java
+++ src/main/java/com/chessgear/data/PGNParser.java
@@ -1,5 +1,9 @@
 package com.chessgear.data;
 
+import com.chessgear.game.Move;
+import com.chessgear.game.Player;
+import com.chessgear.game.Result;
+
 /**
  * Created by Ran on 10/14/2015.
  */
@@ -13,6 +17,21 @@ public class PGNParser {
 
     }
 
+    public String getWhitePlayerName() {
+
+    }
+
+    public String getBlackPlayerName() {
+
+    }
+
+    public Result getResult() {
+
+    }
+
+    public Move getHalfMove(Player player, int fullMoveNumber) {
+
+    }
 
 
 }
