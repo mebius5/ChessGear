a934f05a797ed82253fc50080f239aecd8972247
Abso <rliu14@jhu.edu>
2015-10-18 12:17:32 -0400

    PGN Parsing code.

--- src/test/java/PGNParseTest.java
+++ src/test/java/PGNParseTest.java
@@ -1,5 +1,10 @@
 import org.junit.Test;
 
+import java.util.List;
+
+import static com.chessgear.data.PGNParser.parseTags;
+import static com.chessgear.data.PGNParser.Tag;
+
 /**
  * Created by Ran on 10/18/2015.
  */
@@ -7,6 +12,12 @@ public class PGNParseTest {
 
     @Test
     public void getTagsTest() {
+
+        String testString = "[Event \"Hourly Bullet Arena\"]";
+        List<Tag> tags = parseTags(testString);
+        Tag tag = tags.get(0);
+
+        assert(tag.)
 
 
     }
