3229b7bfb85f0d4a0c04b9e1947e8a24ea0c8835
grady_xiao <xiao.grady@gmail.com>
2015-10-24 22:48:37 -0400

    Created JUnit Test for Piece.java in PieceTest.java

--- src/test/java/BoardStateTest.java
+++ src/test/java/BoardStateTest.java
@@ -23,6 +23,7 @@ public class BoardStateTest {
     @Test
     public void testFEN() {
         assertEquals(defaultBoardState.toFEN(), "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
     }
 
     @Test
