10e3bd773f2bb80c543d566eacdb13b660522e36
grady_xiao <xiao.grady@gmail.com>
2015-11-02 14:51:03 -0500

    Added EngineTest.java. Started on Engine.java

--- src/main/java/com/chessgear/analysis/Engine.java
+++ src/main/java/com/chessgear/analysis/Engine.java
@@ -1,8 +1,53 @@
 package com.chessgear.analysis;
 
+import java.io.*;
+
 /**
  * Class to facilitate engine analysis.
  * Created by Ran on 10/24/2015.
  */
 public class Engine {
+    Runtime rt;
+    Process proc;
+
+    BufferedReader stdInput;
+    BufferedReader stdError;
+
+    public Engine(){
+    }
+
+    public void executeThis(){
+        try {
+            rt = Runtime.getRuntime();
+            String command[] = {"cd", "./stockfish-6-src"};
+            proc = rt.exec(command);
+
+            OutputStreamWriter stdOutput = new OutputStreamWriter(proc.getOutputStream());
+            stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+
+            stdOutput.write("ls");
+            stdOutput.flush();
+
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            String s;
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+            stdInput.close();
+            stdError.close();
+            stdOutput.close();
+
+        } catch (IOException e){
+            System.err.print(e.getMessage());
+        }
+    }
+
 }
---
+++ src/test/java/EngineTest.java
@@ -0,0 +1,17 @@
+/**
+ * Created by GradyXiao on 11/2/15.
+ */
+
+import static org.junit.Assert.*;
+
+import com.chessgear.analysis.Engine;
+import org.junit.Test;
+
+
+public class EngineTest {
+    @Test
+    public void testExecuteThis(){
+        Engine engine=new Engine();
+        engine.executeThis();
+    }
+}
