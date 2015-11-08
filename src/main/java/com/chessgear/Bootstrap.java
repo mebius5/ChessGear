715299e68b4810497efb828754724506d0a28cf8
neilfendley <neilfendley@gmail.com>
2015-11-08 15:31:33 -0500

    Adding logging in

--- src/main/java/com/chessgear/Bootstrap.java
+++ src/main/java/com/chessgear/Bootstrap.java
@@ -1,6 +1,15 @@
 package com.chessgear;
 
+import com.chessgear.data.DatabaseService;
 import com.chessgear.server.ChessGearServer;
+import com.chessgear.server.User;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+
+import java.io.IOException;
+import java.util.*;
 
 import static spark.Spark.*;
 
@@ -13,12 +22,31 @@ public class Bootstrap {
     /**
      * Port for server to listen on.
      */
-    private static final int PORT = 80;
+    private static final int PORT = 8080;
 
-    /**s
+    /**
+     * The Database for the program
+     */
+    private DatabaseService database;
+
+
+    /**
      * Address of server.
      */
     private static final String ADDRESS = "localhost";
+
+    static public DatabaseService dirty() {
+        DatabaseService data = null;
+        try {
+            data = new DatabaseService("neiltest");
+        } catch (IOException | IllegalArgumentException b) {
+            System.out.println("Failure to connect to database");
+        }
+
+        return data;
+    }
+
+
 
     public static void main (String[] args) {
 
@@ -27,15 +55,66 @@ public class Bootstrap {
 
         port(PORT);
         ipAddress(ADDRESS);
-
+        //neiltest
+        final DatabaseService data = dirty();
+        HashMap<User.Property, String > map = new HashMap<>();
+        //data.addUser("email@email.com", map);
         // Handle login
         post("/chessgear/api/login", (request, response) -> {
-            return ""; // TODO
+            String temp = request.body();
+            JsonParser parsed = new JsonParser();
+            JsonObject user = parsed.parse(temp).getAsJsonObject();
+            String email = user.get("email").getAsString();
+            if (data.userExists(email)) {
+                String pass = user.get("password").getAsString();
+                System.out.println(pass);
+                Map<User.Property, String> maps = data.fetchUserProperties(email);
+                String corr = maps.get(User.Property.PASSWORD);
+                System.out.println(corr);
+                if (corr.equals(pass)) {
+                    response.status(200);
+                } else {
+                    JsonObject error = new JsonObject();
+                    error.addProperty("why", "Incorrect Password");
+                    response.status(408);
+                    return error;
+                }
+            } else {
+                JsonObject error = new JsonObject();
+                error.addProperty("why", "User Does Not exist");
+                response.status(408);
+                return error;
+            }
+            return ""; //TODO
         });
 
         // Handle register
         put("/chessgear/api/register", (request, response) -> {
-            return ""; // TODO
+            String temp = request.body();
+            JsonParser parsed = new JsonParser();
+            JsonObject user = parsed.parse(temp).getAsJsonObject();
+            String email = user.get("email").getAsString();
+            if(!data.userExists(email)) {
+                String pass = user.get("password").getAsString();
+                HashMap<User.Property, String> prop = new HashMap<>();
+                prop.put(User.Property.PASSWORD, pass);
+                try {
+                    data.addUser(email, prop);
+                } catch (IllegalArgumentException e) {
+                    response.status(409);
+                    JsonObject error = new JsonObject();
+                    error.addProperty("why", "Incorrect Format");
+                    return error;
+                }
+                JsonObject ret = new JsonObject();
+                ret.addProperty("email", email);
+                return ret;
+            } else {
+                response.status(409);
+                JsonObject error = new JsonObject();
+                error.addProperty("why", "User already exists");
+                return error;
+            }
         });
 
         // Handle tree retrieval
---
+++ src/test/java/BootStrapTest.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by neil on 11/8/15
+ */
+public class BootStrapTest {
+    @Before
+    public void initialize () {
+
+    }
+}
