a08918418d2e6a47c0224005c82eaecfe16f9783
grady_xiao <xiao.grady@gmail.com>
2015-11-08 01:46:15 -0500

    Added functionality to Engine.java to parse cp, pv, and bestmove from FEN string. Modified the corresponding EngineTest.java as well.

--- src/main/java/com/chessgear/analysis/Engine.java
+++ src/main/java/com/chessgear/analysis/Engine.java
@@ -1,6 +1,8 @@
 package com.chessgear.analysis;
 
 import java.io.*;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
 
 /**
  * Class to facilitate engine analysis.
@@ -11,43 +13,95 @@ public class Engine {
     Process proc;
 
     BufferedReader stdInput;
-    BufferedReader stdError;
+    BufferedWriter stdOutput;
 
+    int cp; //Stores the cp score returned by the Stockfish engine
+    String pv; //Stores the pv moves returned by the Stockfish engine
+    String bestMove; //Stores the best move returned by the Stockfish engine
+
+    /***
+     * Default constructor. Starts the Stockfish engine
+     */
     public Engine(){
-    }
-
-    public void executeThis(){
         try {
             rt = Runtime.getRuntime();
-            String command[] = {"cd", "./stockfish-6-src"};
-            proc = rt.exec(command);
+            proc = rt.exec(new String[]{"./stockfish-6-src/src/./stockfish"});
 
-            OutputStreamWriter stdOutput = new OutputStreamWriter(proc.getOutputStream());
+            stdOutput = new BufferedWriter(new OutputStreamWriter(proc.getOutputStream()));
             stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 
-            stdOutput.write("ls");
+    /***
+     * Analyses the FEN string with moveTime in ms as input. Sets the cp, pv, and bestMove values.
+     * @param fen the FEN string passed in
+     * @param moveTime the time for the engine to analyse for in ms
+     */
+    public void analyseFEN(String fen,int moveTime){
+        try {
+            String command;
+            command="position fen "+fen+"\n";
+            stdOutput.write(command);
             stdOutput.flush();
 
+            command="go movetime "+moveTime+"\n";
+            stdOutput.write(command);
+            stdOutput.flush();
+
+            /***
+            command="quit"+"\n";
+            stdOutput.write(command);
+            stdOutput.flush();
+            ****/
+
             // read the output from the command
-            System.out.println("Here is the standard output of the command:\n");
-            String s;
+            //System.out.println("Here is the standard output of the command:\n");
+            String s="";
+            Scanner scanner=new Scanner(s);
+            MatchResult matchResult;
             while ((s = stdInput.readLine()) != null) {
-                System.out.println(s);
+                //System.out.println(s); //
+                if(s.contains("cp")){
+                    scanner = new Scanner(s);
+                    scanner.findInLine("cp (\\d+)");
+                    matchResult = scanner.match();
+                    cp = Integer.parseInt(matchResult.group(1));
+                }
+                if(s.contains("pv")){
+                    pv=s.substring(s.indexOf(" pv ")+4);
+                }
+                if(s.contains("bestmove")) {
+                    scanner = new Scanner(s);
+                    scanner.findInLine("bestmove (\\w+)");
+                    matchResult = scanner.match();
+                    bestMove = matchResult.group(1);
+                    break;
+                }
             }
 
-            // read any errors from the attempted command
-            System.out.println("Here is the standard error of the command (if any):\n");
-            while ((s = stdError.readLine()) != null) {
-                System.out.println(s);
-            }
+            /***
+             * //Display results
+            System.out.println("Last cp: "+cp);
+            System.out.println("Last pv: "+pv);
+            System.out.println("Best move was "+bestMove);
+             ***/
+
+            scanner.close();
             stdInput.close();
-            stdError.close();
             stdOutput.close();
-
-        } catch (IOException e){
-            System.err.print(e.getMessage());
+        } catch (Exception e){
+            e.printStackTrace();
         }
+    }
+
+    /***
+     * Accessor for the best move
+     * @return bestMove found by the Stockfish engine
+     */
+    public String getBestMove(){
+        return this.bestMove;
     }
 
 }
--- src/test/java/EngineTest.java
+++ src/test/java/EngineTest.java
@@ -9,9 +9,13 @@ import org.junit.Test;
 
 
 public class EngineTest {
+    Engine engine = new Engine();
+
     @Test
-    public void testExecuteThis(){
-        Engine engine=new Engine();
-        engine.executeThis();
+    public void testAnalyseFEN(){
+        String fen = "rnbqk1r1/ppppbppp/5n2/4p3/4P3/5N2/PPPPBPPP/RNBQ1RK1 w q - 6 5";
+        int moveTime = 5000;
+        engine.analyseFEN(fen,moveTime);
+        assertEquals(engine.getBestMove(),"f3e5");
     }
 }
