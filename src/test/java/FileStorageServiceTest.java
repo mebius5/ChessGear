cb1741e04e50b7e1ab5a0784531d2e8c1e69a37e
Gilbert Maystre <gilbert@maystre.ch>
2015-12-07 13:25:06 -0500

    Added basic skeleton test

---
+++ src/test/java/FileStorageServiceTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/*
+ *	Author:      Gilbert Maystre
+ *	Date:        Dec 7, 2015
+ */
+
+public class FileStorageServiceTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
